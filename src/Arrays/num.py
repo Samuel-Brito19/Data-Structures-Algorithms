ce659cba7596611dd2c20ae095e53d54f9b6fe25
Samuel-Brito19 <sambriton7@gmail.com>
2024-04-22 20:55:07 -0300

    Trying python

---
+++ src/Arrays/num.py
@@ -0,0 +1,10 @@
+
+def containsDuplicate(nums):
+        tuple = set()
+        for n in nums:
+            if n in tuple:
+                return True
+            tuple.add(n)
+        return False
+
+print(containsDuplicate([1,4,6,7]))
