f4006efee1ed9d339628d9a5037d2f6cd104ab1e
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-11 20:44:06 -0300

    Dictionary chatGPT

---
+++ src/Implementations/Dictionaries/dictionary.py
@@ -0,0 +1,31 @@
+class MyDictionary:
+    def __init__(self):
+        self.items = {}
+
+    def set(self, key, value):
+        self.items[key] = value
+
+    def get(self, key):
+        return self.items.get(key)
+
+    def remove(self, key):
+        if key in self.items:
+            del self.items[key]
+
+    def keys(self):
+        return list(self.items.keys())
+
+    def values(self):
+        return list(self.items.values())
+
+    def items(self):
+        return list(self.items.items())
+
+    def has_key(self, key):
+        return key in self.items
+
+    def clear(self):
+        self.items.clear()
+
+    def size(self):
+        return len(self.items)
