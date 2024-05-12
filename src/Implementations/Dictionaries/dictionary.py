b5813fc5fce44b433f24d317755588d83326c6bf
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-12 20:14:25 -0300

    Implement dictionaries with my G Gomes!

--- src/Implementations/Dictionaries/dictionary.py
+++ src/Implementations/Dictionaries/dictionary.py
@@ -1,40 +1,73 @@
+class ValuePair:
+    def __init__(self, key, value) -> None:
+        self.key = key
+        self.value = value
+
+
 class MyDictionary:
-    def __init__(self):
-        self.items = {}
+    def __init__(self, to_str_fn=str) -> None:
+        self.to_str_fn = to_str_fn
+        self.table: dict[str, ValuePair] = {}
 
     def set(self, key, value):
-        self.items[key] = value
+        if key is not None and value is not None:
+            table_key = self.to_str_fn(key)
+            self.table[table_key] = ValuePair(key, value)
+            return True
+        return False
 
     def get(self, key):
-        return self.items.get(key)
-
-    def remove(self, key):
-        if key in self.items:
-            del self.items[key]
-
-    def keys(self):
-        return list(self.items.keys())
-
-    def values(self):
-        return list(self.items.values())
-
-    def items(self):
-        return list(self.items.items())
+        value_pair = self.table.get(self.to_str_fn(key))
+        return None if value_pair is None else value_pair.value
 
     def has_key(self, key):
-        return key in self.items
+        return self.table.get(self.to_str_fn(key)) is not None
 
-    def clear(self):
-        self.items.clear()
+    def remove(self, key):
+        if self.has_key(key):
+            del self.table[self.to_str_fn(key)]
+            return True
+        return False
+
+    def key_values(self):
+        return self.table.values()
+
+    def values1(self):
+        values = []
+        for key_value in self.key_values():
+            values.append(key_value.value)
+        return values
+
+    def values2(self):
+        return list(map(lambda key_value: key_value.value, self.key_values()))
+
+    def keys(self):
+        keys = []
+        for key_value in self.key_values():
+            keys.append(key_value.key)
+        return keys
+
+    def for_each(self, callback_fn):
+        value_pairs = self.key_values()
+        for value_pair in value_pairs:
+            result = callback_fn(value_pair.key, value_pair.value)
+            if not result:
+                break
+
+    def is_empty(self):
+        return len(self) == 0
 
     def size(self):
-        return len(self.items)
+        return len(self.table.keys)
 
+    def clear(self):
+        self.table = {}
 
-test = MyDictionary()
-test.set('neymar', 'craque')
-test.set('messi', 'argentina')
-print(test.has_key('neymar'))
-print(test.keys())
-print(test.values())
-print(test.get('messi'))
+    def to_string(self):
+        if self.is_empty():
+            return ""
+        value_pairs = self.key_values()
+        obj_string = str(value_pairs[0])
+        for pair in value_pairs:
+            obj_string += str(pair)
+        return obj_string
