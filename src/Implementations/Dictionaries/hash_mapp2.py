e2779a9675d2d92ec3ebd10f7ba3642995c468fc
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-14 11:50:56 -0300

    loselosehash function

--- src/Implementations/Dictionaries/hash_mapp2.py
+++ src/Implementations/Dictionaries/hash_mapp2.py
@@ -8,3 +8,12 @@ class HashMap:
     def __init__(self, to_str_fn=str) -> None:
         self.to_str_fn = to_str_fn
         self.table: dict[str, ValuePair] = {}
+
+    def lose_lose_hash_code(self, key):
+        if isinstance(key, (int)):
+            return key
+        table_key = self.to_str_fn(key)
+        hash_number = 0
+        for i in range(len(table_key)):
+            hash_number += ord(table_key[i])
+        return hash_number % 37
