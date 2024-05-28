3265cab2cec3164d8c17e50722a6c20be9b483ce
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-28 08:48:58 -0300

    Group anagrams

--- src/Questions/Arrays/group_anagrams.py
+++ src/Questions/Arrays/group_anagrams.py
@@ -1,2 +1,15 @@
-def group_anagrams(self, strs: list[str]):
-    group_anagram = defaultdict()
+from collections import defaultdict
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        group_anagram = defaultdict(list)
+        result = []
+
+        for s in strs:
+            sorted_s = tuple(sorted(s))
+            group_anagram[sorted_s].append(s)
+
+        for value in group_anagram.values():
+            result.append(value)
+        return result
