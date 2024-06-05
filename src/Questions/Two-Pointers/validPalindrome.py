e1049522ad62680b899c01633091b81be765d72b
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-05 10:39:10 -0300

    Valid palindrome new solution

--- src/Questions/Two-Pointers/validPalindrome.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        newStr = ""
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
