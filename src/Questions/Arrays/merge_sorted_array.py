11c162df3d6199eaba089ff39f965aa6ba367aad
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-23 20:44:31 -0300

    1D array

--- src/Questions/Arrays/merge_sorted_array.py
+++
@@ -1,9 +0,0 @@
-def runningSum(self, nums: list):
-    result = []
-    for n in range(nums):
-        a = nums[n] + nums[n + 1]
-        n += 1
-    return print(a)
-
-
-print(runningSum([1, 2, 3, 4]))
